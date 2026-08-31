6d393cd25314ef921dccad155236f14e1fcae0c5
Pedro Americo <dev.americo@gmail.com>
2026-08-31 07:52:08 -0300

    feat: caixa seletora

--- front/interface.py
+++ front/interface.py
@@ -10,4 +10,16 @@ janela.configure(padx=20, pady=20)
 titulo = tk.Label(janela, text="Sistema de Logística - DF", font=("Arial", 16, "bold"))
 titulo.pack(pady=10)
 
+# Selecionar o tipo de busca
+frame_seletor = tk.Frame(janela)
+frame_seletor.pack(pady=5)
+
+var_tipo_busca = tk.StringVar(value="cep")
+
+rb_cep = tk.Radiobutton(frame_seletor, text="Busca por CEP (Hash)", variable=var_tipo_busca, value="cep")
+rb_cep.pack(side=tk.LEFT, padx=10)
+
+rb_rua = tk.Radiobutton(frame_seletor, text="Busca por Rua (Binária)", variable=var_tipo_busca, value="rua")
+rb_rua.pack(side=tk.LEFT, padx=10)
+
 janela.mainloop()
